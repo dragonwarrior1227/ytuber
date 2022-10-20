bf14cd875b3feb53ad64f9d3e4406aabf8dee66d
dragonwarrior1227 <vinay2210978@gmail.com>
2022-10-20 23:31:26 +0200

    utube

--- fullsetup.py
+++ fullsetup.py
@@ -19,11 +19,11 @@ from xvfbwrapper import Xvfb
 
 
 
-print(subprocess.Popen("npm install chrome -g",shell=True,stdout=subprocess.PIPE).communicate()[0])
-print(subprocess.Popen("chrome --version",shell=True,stdout=subprocess.PIPE).communicate()[0])
-# print(subprocess.Popen("npm install chromium-version@77",shell=True,stdout=subprocess.PIPE).communicate()[0])
+# print(subprocess.Popen("npm install chrome -g",shell=True,stdout=subprocess.PIPE).communicate()[0])
+# print(subprocess.Popen("chrome --version",shell=True,stdout=subprocess.PIPE).communicate()[0])
+print(subprocess.Popen("npm install chromium-version@77",shell=True,stdout=subprocess.PIPE).communicate()[0])
 chrome_path=r"{}/node_modules/chromium-version/lib/chromium/chrome-linux/chrome".format(os.getcwd())
-chrome_path=r"/opt/buildhome/.nvm/versions/node/v12.18.0/bin/chrome"
+# chrome_path=r"/opt/buildhome/.nvm/versions/node/v12.18.0/bin/chrome"
 
 print(subprocess.Popen("npm install xvfb",shell=True,stdout=subprocess.PIPE).communicate()[0])
 print(subprocess.Popen("whereis xvfb",shell=True,stdout=subprocess.PIPE).communicate()[0])
