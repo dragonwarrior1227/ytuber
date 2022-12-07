7b48d775f3d2edd2914d124b3566e4bc97082ab5
dragonwarrior1227 <vinay2210978@gmail.com>
2022-12-07 21:51:07 +0100

    test

--- ver_fullsetup.py
+++ ver_fullsetup.py
@@ -24,7 +24,7 @@ from xvfbwrapper import Xvfb
 # print(subprocess.Popen("npm install chrome -g",shell=True,stdout=subprocess.PIPE).communicate()[0])
 # print(subprocess.Popen("chrome --version",shell=True,stdout=subprocess.PIPE).communicate()[0])
 print(subprocess.Popen("npm install chromium",shell=True,stdout=subprocess.PIPE).communicate()[0])
-chrome_path=r"{}/node_modules/chromium-version/lib/chromium/chrome-linux/chrome".format(os.getcwd())
+chrome_path=r"{}/node_modules/chromium/lib/chromium/chrome-linux/chrome".format(os.getcwd())
 
 print(subprocess.Popen("npm install xvfb",shell=True,stdout=subprocess.PIPE).communicate()[0])
 print(subprocess.Popen("whereis xvfb",shell=True,stdout=subprocess.PIPE).communicate()[0])
